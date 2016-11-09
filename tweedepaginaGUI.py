65dfd237eb542e3899ebddd3c3f2de2f2945f0c9
Lennart <lennartheinen@hotmail.com>
2016-11-09 14:29:27 +0100

    1ste pagina verbonden met de tweede pagina

--- tweedepaginaGUI.py
+++ tweedepaginaGUI.py
@@ -1,13 +1,31 @@
 from tkinter import *
 from tkinter.messagebox import showinfo
+from API_test_marvel import *
+import textwrap
 import random
 
 root = Tk()   # maakt het hoofdscherm
 
+lijst1 = result
+NaamHeld = lijst1[0].get('name')
+IdHeld = lijst1[0].get('id')
+BeschrijvingHeld = lijst1[0].get('description')
+
+
+#if NaamHeld == 'Iron Man':
+   # BeschrijvingHeld2 = BeschrijvingHeld.replace('Tony Stark', '-----')
+  #  if NaamHeld in BeschrijvingHeld2:
+ #       BeschrijvingHeld2 = BeschrijvingHeld.replace(NaamHeld, '-----')
+
+if NaamHeld in BeschrijvingHeld:
+    BeschrijvingHeld2 = BeschrijvingHeld.replace(NaamHeld, '-----')
+
+
+
 # pop-up checked of iron man wordt opgegeven
 def clicked():
     invoer = entry.get()
-    if invoer == 'iron man':
+    if invoer == NaamHeld:
         bericht = 'Je hebt het goed'
         showinfo(title='Goed', message=bericht)
     else:
@@ -24,22 +42,27 @@ def clicked2():
     pass
 #button 3
 def clicked3():
-    print("Hello")
-    pass
+    label = Label(master=root,
+              text=BeschrijvingHeld2,
+              background='red',
+              font=('Helvetica', 10, 'bold italic'),
+              height=10,
+                width=100,
+              wraplength = 500 )
+    label.pack()
+
+
+
 
 label = Label(master=root,
-              text='Hier komt een random message te staan over de superheld',
+              text='Hint#1',
               background='red',
+              command=clicked3(),
               font=('Helvetica', 10, 'bold italic'),
               height=3)
 label.pack()
 
-label2 = Label(master=root,
-              text='vul cijfer in voor een kwadraat',
-              background='red',
-              font=('Helvetica', 10, 'bold italic'),
-              height=3)
-label2.pack()
+#labeltext= textwrap.fill(labeltext,width=30)
 
 #rechterframe = Frame(master=root)
 #rechterframe.pack(side=RIGHT)
