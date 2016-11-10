adf37141a39e5f28faac9bf265fac07880985427
Lennart <lennartheinen@hotmail.com>
2016-11-10 14:48:43 +0100

    update tweedepagina

--- tweedepaginaGUI.py
+++ tweedepaginaGUI.py
@@ -2,10 +2,11 @@ from tkinter import *
 from tkinter.messagebox import showinfo
 from API_test_marvel import *
 from time import sleep
-#import datetime
 
 #Maakt het hoofdscherm
 root = Tk()
+
+punten = 25
 
 # result wordt opgehaald en in een lijst gezet
 lijst1 = result
@@ -13,66 +14,34 @@ NaamHeld = lijst1[0].get('name')
 IdHeld = lijst1[0].get('id')
 BeschrijvingHeld = lijst1[0].get('description')
 
-
-#if NaamHeld == 'Iron Man':
-   # BeschrijvingHeld2 = BeschrijvingHeld.replace('Tony Stark', '-----')
-  #  if NaamHeld in BeschrijvingHeld2:
- #       BeschrijvingHeld2 = BeschrijvingHeld.replace(NaamHeld, '-----')
-
 if NaamHeld in BeschrijvingHeld:
     BeschrijvingHeld2 = BeschrijvingHeld.replace(NaamHeld, '-----')
 
-lab = Label(root)
-lab.pack()
+
 # aantal punten bij het begin
 # pop-up checked of iron man wordt opgegeven
 def clicked():
-    punten = 25
+    global punten
     invoer = entry.get()
     if invoer == NaamHeld:
         bericht = 'Je hebt het goed'
         showinfo(title='Goed', message=bericht)
     else:
         bericht = 'Je hebt het fout'
-       # lab.config(text=punten)
-      #  lab['text'] = punten
-
-        punten -= 5
+        punten -= 1
         showinfo(title='Fout', message=bericht)
-        label3["text"] =  punten
-    #root.after(1000, clicked)
-    return punten
-#clicked()
+        label3['text'] = punten
 
 #achtergrond wordt rood gemaakt
 root.configure(background='red')
 
-# timer
-#lab = Label(root)
-#lab.pack()
-
-#def clock():
- #   time = 0
- #   while time != 20:
-     #   time += 1
-     #   lab.config(text=time)
-     #   lab['text'] = time
-        #root.after(1000, clock) # run itself again after 1000 ms
-    #sleep(1)
-#  run first time
-#clock()
-
-#button 2
-def clicked2():
-    pass
-
 label3 = Label(master=root,
               background='red',
-              text='punten',
+              text=punten,
               wraplength = 500,
               height=3,
               padx = 50)
-label3.pack()
+label3.grid(row=0,column=5)
 
 #Titel label ( wordt veranderd )
 Titel = Label(master=root,
@@ -83,7 +52,8 @@ Titel = Label(master=root,
               height=6,
 
               padx = 50)
-Titel.pack()
+Titel.grid(row=1,column=5)
+
 
 
 # De Question knop
@@ -92,52 +62,62 @@ def QuestionKnop():
     showinfo(title='Informatie', message=bericht)
 
 # de Functie voor de hintknop
-def HintKnop(punten):
-    label["text"] = punten
-    punten -=3
-    return punten
-
+def HintKnop():
+    global punten
+    Hint1["text"] = BeschrijvingHeld2
+    punten -= 3
+    label3['text'] = punten
 # de Hint labellen
 Hint1 = Label(master=root,
               background='red',
               text='Hint 1',
               wraplength = 500,
-              height=3,
+              height=6,
               padx = 50)
-Hint1.pack()
+Hint1.grid(row=2,column=5)
 Hint2 = Label(master=root,
               background='red',
               text='Hint 2',
               wraplength = 500,
               height=3,
               padx = 50)
-Hint2.pack()
+Hint2.grid(row=3,column=5)
 Hint3 = Label(master=root,
               background='red',
               text='Hint 3',
               wraplength = 500,
               height=3,
               padx = 50)
-Hint3.pack()
+Hint3.grid(row=4,column=5)
 
-# de randomize button
-Randomize = Button(master=root, text='punten', command=clicked2)
-Randomize.pack(pady=10)
+#bottomframe= Frame(master=root)
+#bottomframe.grid(row=6,column=5)
+
+
+Naam = Label(master=root,
+              background='red',
+              text='Hier komt de naam',   # variable waar de naam inzit met input.
+              wraplength = 500,
+              font=(10),
+              height=6,
+              padx = 50)
+Naam.grid(row=3,column=0)
 
 # de invoer button
-InvoerButton = Button(master=root, text='       Invoer       ', command=clicked)
-InvoerButton.pack(pady=10,ipadx=50)
+InvoerButton = Button(master=root, text='       Enter       ', command=clicked)
+InvoerButton.grid(row=6,column=6)
 
 #de Hint button
 HintButton = Button(master=root, text='        Hint       ', command=HintKnop)
-HintButton.pack(pady=10, ipadx=50)
+HintButton.grid(row=6,column=4)
 
 #de Help button ( spelregels )
-HintButton = Button(master=root, text='?', command=QuestionKnop)
-HintButton.pack(pady=10, ipadx=50)
-# de entry button (input)
+QuestionButton = Button(master=root, text='?', command=QuestionKnop)
+QuestionButton.place(x=1500,y=0)
+
+# de entry balk (input)
 entry = Entry(master=root)
-entry.pack(padx=10, pady=10)
+entry.grid(row=5,column=5)
 
 # toon het hoofdscherm
 root.mainloop()
